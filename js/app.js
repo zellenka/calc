04f09f3b0f569b30d131b123f7e3d0b345ff1506
Serhii <seleshhenko@yahoo.com>
2018-07-10 12:53:40 +0300

    bootstrap

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
 let app = new Vue ({
     el: '#app',
     data: {
-        message: 'Запоните пустые поля'
+        message: 'Запоните пустые поля',
+        messageWeight: 'Вес в грпммах, например 500',
+        count: 1,
+        EMS: 10,
+        koef: '4.3'
         
     },
     watch: {
         
     },
     methods: {
-        result: function () {
+        Price: function () {
             let itemPrice = Number(document.getElementById("itemPrice").value);
             let emc = Number(document.getElementById("EMC").value);
             let amount = Number(document.getElementById("amount").value);
             let koef = Number(document.getElementById("koef").value);
-            var f = ((amount * itemPrice + emc) * 0.1 + (amount * itemPrice + emc))*koef;
-            var x = (f * 0.02 + f) + ((f * 0.02 + f) * 0.22);
+            let f = ((amount * itemPrice + emc) * 0.1 + (amount * itemPrice + emc))*koef;
+            let x = (f * 0.02 + f) + ((f * 0.02 + f) * 0.22);
             this.message = x.toFixed(2) + ' UAH';
-          }
+          },
+        Weight: function() {
+            let itemweight = Number(document.getElementById("itemweight").value);
+            let Wresult = itemweight * 0.01 * 37.8;
+            this.messageWeight = Wresult.toFixed(2) + ' UAH';
+        }
+        
     }
 })
 
